08bd632fc8b846c3e91f86f8788ee5d3bd27fb75
Rodrigo Corozo <rcorozo@orocash.ec>
2022-10-31 15:53:28 -0500

    Update main.go to return the specified JSON payload and the ERROR string when NotFoundHandler

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
+type userMessage struct {
+	Message       string `json:"message"`
+	From          string `json:"from"`
+	To            string `json:"to"`
+	TimeToLifeSec int    `json:"timeToLifeSec"`
+}
+
+type serverReplyMessage struct {
+	ReplyMessage string `json:"message"`
+}
+
 func devOps(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "BPICHE - DevOps Technical Assessment")
+	var newMessage userMessage
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Keyboard not found. Press a button to Resume.")
+	}
+
+	json.Unmarshal(reqBody, &newMessage)
+	//w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+
+	var newReply serverReplyMessage
+	newReply.ReplyMessage = "Hello " + newMessage.From + " your message will be send."
+
+	json.NewEncoder(w).Encode(newReply)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+	fmt.Fprintf(w, "ERROR")
+	return
 }
 
 func handleRequests() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/DevOps", devOps)
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/DevOps", devOps)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
-    handleRequests()
+	handleRequests()
 }
