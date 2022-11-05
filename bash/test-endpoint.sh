bdc1a8e24682b65b3819effbc83e9fc8e8b1d9c4
Rodrigo Corozo <rcorozo@orocash.ec>
2022-11-05 13:21:06 -0500

    Update k8s/02-ingress.yaml to handle SSL certs, Update k8s/01-deployment-restful-api.yaml to inject Secrets as ENV variables, Add k8s/03-ssl-issuer.yaml for SSL certs issuing

--- bash/test-endpoint.sh
+++ bash/test-endpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HOST="restapi.rodrigocorozo.com"
-JWT=""
+JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJjbGllbnQiOiJCUElDSEUiLCJleHAiOjE2Njc2NzI4NTZ9.UHoUlOrFqkYSZZ1SYzfNBjYzmPiUyauLbj_r6Xzpuoo"
 
 curl -X POST \
 -H "X-Parse-REST-API-Key: 2f5ae96c-b558-4c7b-a590-a501ae1c3f6c" \
 -H "X-JWT-KWY: ${JWT}" \
 -H "Content-Type: application/json" \
 -d '{ "message" : "This is a test", "to": "Juan Perez", "from": "Rita Asturia", "timeToLifeSec" : 45 }' \
-http://${HOST}:8080/DevOps
+https://${HOST}/DevOps
