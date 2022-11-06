12281bf2f9ff2015c37d832c7f204283fcc6add4
Rodrigo Corozo <rcorozo@orocash.ec>
2022-11-06 06:46:48 -0500

    Minor changes

--- bash/test-endpoint.sh
+++ bash/test-endpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HOST="restapi.rodrigocorozo.com"
-JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJjbGllbnQiOiJCUElDSEUiLCJleHAiOjE2Njc2NzkzNTF9.NUcfiHmo1r8q46uDTD90FxobxNzjYCp59n6X8Z-ZdvI"
+JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJjbGllbnQiOiJCUElDSEUiLCJleHAiOjE2Njc3MzYzNzZ9.tvGu2XF_HC8lbDXHfJy9daQzoz-sL2p88y8-HcWZxxo"
 
 curl -X POST \
 -H "X-Parse-REST-API-Key: 2f5ae96c-b558-4c7b-a590-a501ae1c3f6c" \
 -H "X-JWT-KWY: ${JWT}" \
 -H "Content-Type: application/json" \
--d '{ "message" : "This is a test", "to": "Juan Perez", "from": "Rita Asturia", "timeToLifeSec" : 45 }' \
+-d '{ "message" : "This is a test", "to": "Juan Perez", "from": "Rodrigo Corozo", "timeToLifeSec" : 45 }' \
 https://${HOST}/DevOps
