f044bf569bf5a6fad120e85fcca5bfeed48413dd
Rodrigo Corozo <rcorozo@riseup.net>
2023-02-26 18:53:58 -0500

    Update tf github secrets

--- tf/github.tf
+++ tf/github.tf
@@ -4,7 +4,7 @@ resource "github_actions_secret" "base64-KUBE_CONFIG" {
   plaintext_value  = linode_lke_cluster.this.kubeconfig
 }
 
-resource "github_actions_secret" "this" {
+resource "github_actions_secret" "plain-KUBE_CONFIG" {
   repository       = "devops-exercise"
   secret_name      = "plain_KUBE_CONFIG"
   plaintext_value  = base64decode(linode_lke_cluster.this.kubeconfig)
