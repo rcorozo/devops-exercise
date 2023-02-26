e88e78cfb1f86361c9367eb9023433ea88676807
Rodrigo Corozo <rcorozo@riseup.net>
2023-02-26 16:14:26 -0500

    Add k8s tf resource

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -4,6 +4,4 @@ output "api_endpoints" {
 
 output "kubeconfig" {
     value     = nonsensitive(local_file.kubeconfig.content)
-  # value     = nonsensitive(linode_lke_cluster.this.kubeconfig)
-  # sensitive = true
 }
