223d2a013f5855c91c08c34ae2d643013c8e4509
Rodrigo Corozo <rcorozo@riseup.net>
2023-02-26 17:50:22 -0500

    Update github resource

--- tf/github.tf
+++ tf/github.tf
@@ -1,4 +1,4 @@
-resource "github_actions_secret" "example_secret" {
+resource "github_actions_secret" "this" {
   repository       = "devops-exercise"
   secret_name      = "KUBE_CONFIG"
   plaintext_value  = base64decode(linode_lke_cluster.this.kubeconfig)
